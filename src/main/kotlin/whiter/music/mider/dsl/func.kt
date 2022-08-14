657364e66d77c24cdd2ddeb0903e4f7a47fac0a3
whiterasbk <whiterasbk@qq.com>
2022-08-14 08:34:19 +0800

    删除无用代码

--- src/main/kotlin/whiter/music/mider/dsl/func.kt
+++ src/main/kotlin/whiter/music/mider/dsl/func.kt
@@ -12,16 +12,6 @@ import javax.sound.midi.Sequencer
 import kotlin.concurrent.timerTask
 import kotlin.math.log2
 
-//@Deprecated(message = "contents will be replaced")
-//fun play(autoClose: Boolean = true, block: MiderDSL.() -> Any) {
-//    val pair = playAsync(false, block)
-//    Thread.sleep(pair.first)
-//
-//    if (autoClose) {
-//        pair.second.close()
-//    }
-//}
-
 fun play(autoClose: Boolean = true, block: MiderDSL.() -> Unit) {
     val pair = playAsync(false, block)
     Thread.sleep(pair.first)
@@ -30,24 +20,6 @@ fun play(autoClose: Boolean = true, block: MiderDSL.() -> Unit) {
         pair.second.close()
     }
 }
-
-//@Deprecated(message = "contents will be replaced")
-//fun playAsync(autoClose: Boolean = false, block: MiderDSL.() -> Any): Pair<Long, Sequencer> {
-//    val sequencer = MidiSystem.getSequencer()
-//    fromDsl(block).inStream().use {
-//        sequencer.setSequence(it)
-//        sequencer.open()
-//        sequencer.start()
-//        val delay: Long = sequencer.sequence.microsecondLength / 1000 + 500
-//
-//        if (autoClose) {
-//            Timer().schedule(timerTask {
-//                sequencer.close()
-//            }, delay)
-//        }
-//        return delay to sequencer
-//    }
-//}
 
 fun playAsync(autoClose: Boolean = false, block: MiderDSL.() -> Unit): Pair<Long, Sequencer> {
     val sequencer = MidiSystem.getSequencer()
@@ -66,16 +38,6 @@ fun playAsync(autoClose: Boolean = false, block: MiderDSL.() -> Unit): Pair<Long
     }
 }
 
-//@Deprecated(message = "contents will be replaced")
-//fun playDslInstance(autoClose: Boolean = true, miderDSL: MiderDSL) {
-//    val pair = playDslInstanceAsync(false, miderDSL)
-//    Thread.sleep(pair.first)
-//
-//    if (autoClose) {
-//        pair.second.close()
-//    }
-//}
-
 fun playDslInstance(autoClose: Boolean = true, miderDSL: MiderDSL) {
     val pair = playDslInstanceAsync(false, miderDSL)
     Thread.sleep(pair.first)
@@ -84,25 +46,6 @@ fun playDslInstance(autoClose: Boolean = true, miderDSL: MiderDSL) {
         pair.second.close()
     }
 }
-
-//@Deprecated(message = "contents will be replaced")
-//fun playDslInstanceAsync(autoClose: Boolean = false, miderDSL: MiderDSL): Pair<Long, Sequencer> {
-//    val sequencer = MidiSystem.getSequencer()
-//    val midiFile = fromDslInstance(miderDSL)
-//    midiFile.inStream().use {
-//        sequencer.setSequence(it)
-//        sequencer.open()
-//        sequencer.start()
-//        val delay: Long = sequencer.sequence.microsecondLength / 1000 + 500
-//
-//        if (autoClose) {
-//            Timer().schedule(timerTask {
-//                sequencer.close()
-//            }, delay)
-//        }
-//        return delay to sequencer
-//    }
-//}
 
 fun playDslInstanceAsync(autoClose: Boolean = false, miderDSL: MiderDSL): Pair<Long, Sequencer> {
     val sequencer = MidiSystem.getSequencer()
@@ -121,45 +64,6 @@ fun playDslInstanceAsync(autoClose: Boolean = false, miderDSL: MiderDSL): Pair<L
         return delay to sequencer
     }
 }
-
-//@Deprecated(message = "contents will be replaced")
-//fun fromDslInstance(mdsl: MiderDSL): MidiFile {
-//    val minimsTicks = 960
-//    val clock: Byte = 18
-//    val midi = MidiFile()
-//    midi.append {
-//        track {
-//            tempo(mdsl.bpm)
-//
-//            mdsl.keySignature?.let {
-//                meta(MetaEventType.META_KEY_SIGNATURE, (abs(it.first.semitone) or 0x80).toByte(), it.second.toByte())
-//            }
-//
-//            mdsl.timeSignature?.let {
-//                meta(MetaEventType.META_TIME_SIGNATURE, it.first.toByte(), log2(it.second.toDouble()).toInt().toByte(), clock, 8)
-//            }
-//
-//            end()
-//        }
-//
-//        track {
-//            changeProgram(mdsl.program.id.toByte())
-//            parseSound(minimsTicks, mdsl.adjustedList())
-//            if (mdsl.otherTracks.isNotEmpty()) {
-//                mdsl.otherTracks.forEach {
-//                    track {
-//                        changeProgram(it.program.id.toByte())
-//                        parseSound(minimsTicks, it.adjustedList())
-//                        end()
-//                    }
-//                }
-//            }
-//            end()
-//        }
-//    }
-//
-//    return midi
-//}
 
 fun fromDslInstance(dsl: MiderDSL): MidiFile {
     val wholeTicks = 960 * 2 * 2
@@ -217,61 +121,11 @@ fun Dsl2MusicXml(dsl: MiderDSL, divisions: Int = 480): MusicXml {
     )
 }
 
-//@Deprecated(message = "contents will be replaced")
-//fun fromDsl(block: MiderDSL.() -> Any): MidiFile {
-//    val mdsl = MiderDSL()
-//    with(mdsl, block)
-//    return fromDslInstance(mdsl)
-//}
-
 fun fromDsl(block: MiderDSL.() -> Unit): MidiFile {
     val dsl = MiderDSL()
     dsl.block()
     return fromDslInstance(dsl)
 }
-
-//@Deprecated(message = "contents will be replaced")
-//fun apply(path: String, block: MiderDSL.() -> Any) {
-//    val mdsl = MiderDSL()
-//    val minimsTicks = 960
-//    val clock: Byte = 18
-//    with(mdsl, block)
-//    val midi = MidiFile()
-//    midi.append {
-//        track {
-//            tempo(mdsl.bpm)
-//
-//            mdsl.keySignature?.let {
-//                meta(MetaEventType.META_KEY_SIGNATURE, (abs(it.first.semitone) or 0x80).toByte(), it.second.toByte())
-//            }
-//
-//            mdsl.timeSignature?.let {
-//                meta(MetaEventType.META_TIME_SIGNATURE, it.first.toByte(), log2(it.second.toDouble()).toInt().toByte(), clock, 8)
-//            }
-//
-//            end()
-//        }
-//
-//        track {
-//            changeProgram(mdsl.program.id.toByte())
-//
-//            parseSound(minimsTicks, mdsl.adjustedList())
-//
-//            end()
-//        }
-//
-//        if (mdsl.otherTracks.isNotEmpty()) {
-//            mdsl.otherTracks.forEach {
-//                track {
-//                    changeProgram(it.program.id.toByte())
-//                    parseSound(minimsTicks, it.adjustedList())
-//                    end()
-//                }
-//            }
-//        }
-//    }
-//    midi.save(path)
-//}
 
 fun apply(path: String, block: MiderDSL.() -> Unit) {
     if (path.endsWith(".xml")) {
