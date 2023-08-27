866960b17ff727af3a8ae6d4a17623c31ad93f24
whiterasbk <whiterasbk@qq.com>
2023-08-27 16:25:09 +0800

    :hammer: impl ByteBuffer in js

--- src/jsMain/kotlin/org/mider/expect/ByteBuffer.js.kt
+++ src/jsMain/kotlin/org/mider/expect/ByteBuffer.js.kt
@@ -1,34 +1,51 @@
 package org.mider.expect
 
+import org.khronos.webgl.Uint8Array
+import org.khronos.webgl.get
+import org.khronos.webgl.set
+
 actual class ByteBuffer actual constructor(capacity: Int) {
+    private val buffer: Uint8Array = Uint8Array(capacity)
+    private var position: Int = 0
+    val prototype: Uint8Array get() = buffer
 
     actual companion object {
-        actual fun allocate(occupied: Int): ByteBuffer {
-            TODO("Not yet implemented")
-        }
+        actual fun allocate(occupied: Int): ByteBuffer = ByteBuffer(occupied)
     }
 
     actual fun put(byte: Byte) {
-        TODO("Not yet implemented")
+        buffer[position] = byte
+        position ++
     }
 
     actual fun put(bytes: ByteArray) {
-        TODO("Not yet implemented")
+        buffer.set(bytes.toTypedArray(), position)
+        position += bytes.size
     }
 
     actual fun put(buff: ByteBuffer) {
-        TODO("Not yet implemented")
+         buffer.set(buff.buffer, position)
+         position += buff.position
     }
 
     actual fun clear() {
-        TODO("Not yet implemented")
+        for (e in 0 ..< position) buffer[e] = 0
     }
 
     actual fun flip() {
-        TODO("Not yet implemented")
+        position = 0
     }
 
     actual fun get(dst: ByteArray, offset: Int, length: Int) {
-        TODO("Not yet implemented")
+        var i = 0
+        for (e in offset ..< length)
+            dst[i++] = buffer[e]
+    }
+
+    fun getByteBuffer(dst: ByteBuffer, offset: Int, length: Int): ByteBuffer {
+        val ret = ByteBuffer(length)
+        for (e in offset ..< length)
+            ret.put(buffer[e])
+        return ret
     }
 }
